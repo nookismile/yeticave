026e573a2c265404d96bfc57e43fb2e3107d3aa6
nookismile <52919340+nookismile@users.noreply.github.com>
2023-01-26 16:44:54 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,3 +13,27 @@ function format_price($price) {
     }
     return "$price ₽";
 }
+
+/**
+ * Возвращает количество целых часов и остатка минут от настоящего времени до даты
+ * @param string $date Дата истечения времени
+ * @return array
+ */
+
+function count_time($date) {
+    date_default_timezone_set('Europe/Moscow');
+    $final_date = date_create($date);
+    $cur_date = date_create("now");
+    $diff = date_diff($final_date, $cur_date);
+    $format_diff = date_interval_format($diff, "%d %H %I");
+    $arr = explode(" ", $format_diff);
+
+    $hours = $arr[0] * 24 + $arr[1];
+    $minutes = intval($arr[2]);
+    $hours = str_pad($hours, 2, "0", STR_PAD_LEFT);
+    $minutes = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+    $res[] = $hours;
+    $res[] = $minutes;
+
+    return $res;
+}
