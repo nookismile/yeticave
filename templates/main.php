e2e45c2ccaab1cb96793925dcc46e65b92d5cd04
nookismile <52919340+nookismile@users.noreply.github.com>
2023-02-07 12:12:05 +0300

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -3,9 +3,9 @@
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
         <!--заполните этот список из массива категорий-->
-        <?php foreach ($categories as $category => $val): ?>
-            <li class="promo__item promo__item--<?= $category; ?>">
-                <a class="promo__link" href="pages/all-lots.html"><?= $val; ?></a>
+        <?php foreach ($categories as $category): ?>
+            <li class="promo__item promo__item--<?= $category["character_code"]; ?>">
+                <a class="promo__link" href="pages/all-lots.html"><?= $category["name_category"]; ?></a>
             </li>
         <?php endforeach; ?>
     </ul>
@@ -19,17 +19,17 @@
         <?php foreach ($goods as $good): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $good['url'] ?>" width="350" height="260" alt="">
+                    <img src="<?= $good['img'] ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $good['category'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($good['name']) ?></a></h3>
+                    <span class="lot__category"><?= $good["name_category"] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($good["title"]) ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= format_price(htmlspecialchars($good['price']))?></span>
+                            <span class="lot__cost"><?= format_price(htmlspecialchars($good["start_price"]))?></span>
                         </div>
-                        <?php $res = count_time(htmlspecialchars($good["expiration"])) ?>
+                        <?php $res = count_time(htmlspecialchars($good["date_finish"])) ?>
                         <div class="lot__timer timer <?php if ($res[0] < 1) : ?>timer--finishing<?php endif; ?>">
                             <?= "$res[0]: $res[1]"; ?>
                         </div>
